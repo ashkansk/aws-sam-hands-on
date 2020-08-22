757c0139f4bd2a1c0306e70e34fb75ca3bb9f0cd
ashkan <a.sadri.k@gmail.com>
2020-08-22 20:02:35 +0430

    + changed dev server port to 9000 + added 3 environments: dev (no name), test, prod + added new ProductList component with static data + added new route for ProductList.vue

---
+++ aws-sam-handson-frontend/src/product/ProductList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div v-for="(p, i) in products" :key="p.code">
+      {{ i }}. {{ p.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'ProductList',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      value: process.env.VUE_APP_SOME_NAME,
+      products: [
+        { code: 'SP-63478', title: 'Apple Airpods', price: '1000', category: 'Electronics', createdBy: 'Ashkan' },
+        { code: 'SP-69439', title: 'Apple TV', price: '3000', category: 'Electronics', createdBy: 'Ashkan' },
+        { code: 'SP-10385', title: 'Nike Shoes', price: '1000', category: 'Clothes', createdBy: 'Ashkan' },
+        { code: 'SP-83658', title: 'Nike T-Shirt', price: '50', category: 'Clothes', createdBy: 'Ashkan' },
+        { code: 'SP-80234', title: 'Sunglasses', price: '900', category: 'Beauty', createdBy: 'Ashkan' }
+      ]
+    }
+  },
+  beforeCreate () {
+    // sdf sdf
+  },
+  created () {
+    // sdf sdf
+  }
+})
+</script>
--- aws-sam-handson-frontend/vue.config.js
+++ aws-sam-handson-frontend/vue.config.js
@@ -1,6 +1,7 @@
 // vue.config.js
 module.exports = {
   devServer: {
+    port: 9000,
     proxy: 'http://localhost:8080'
   }
   // other Vue config options ...
